29fc7e8cc1579726b3346747d3ed843c872444d4
Tanish Garg <tanishgarg@Tanishs-Laptop.lan>
2025-01-02 19:16:35 -0600

    feat: structure for storing data

--- db/migrations/20250103004047_users.sql
+++ db/migrations/20250103004047_users.sql
@@ -1,6 +1,5 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TYPE ROLE AS ENUM ('admin', 'instructor', 'assistant', 'student');
 CREATE TABLE users (
   id uuid PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
