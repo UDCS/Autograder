a50627114dfe860255ac462aa60e7a7558427d0d
Tanish Garg <tanishgarg@Tanishs-Laptop.lan>
2025-01-02 19:16:35 -0600

    feat: structure for storing data

--- db/migrations/20250103011232_grades.sql
+++ db/migrations/20250103011232_grades.sql
@@ -1,14 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE classrooms (
+CREATE TABLE grades (
     id uuid PRIMARY KEY,
-    name VARCHAR(64) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    assignment_id uuid NOT NULL REFERENCES assignments(id),
+    student_id uuid NOT NULL REFERENCES users(id),
+    score INTEGER NOT NULL,
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE classrooms;
+DROP TABLE grades;
 -- +goose StatementEnd
