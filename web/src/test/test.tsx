d257d0b7cfe3a672840a1efddb8efa3e3ee6c068
a2k6 <adrian@panezic.com>
2025-01-31 22:27:40 -0600

    New page for graphical addition of new users
    VERY MUCH A WORK IN PROGRESS, nothing is tied to the backend as of current.

---
+++ web/src/test/test.tsx
@@ -0,0 +1,37 @@
+import { StrictMode } from "react";
+import { createRoot } from 'react-dom/client'
+import styled from "styled-components";
+import Select from 'react-select'
+import './test.css'
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+
+  &:disabled {
+    color: grey;
+    opacity: 0.7;
+    cursor: default;
+  }
+`;
+
+const roleOptions = [
+    {value: "student", label: "Student"},
+    {value: "assistant", label: "Assistant"},
+    {value: "instructor", label: "Instructor"},
+];
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <div className="addStudentFields">
+            <input placeholder="Enter an email"/>
+            <Select options={roleOptions} placeholder="Select a role" id="dropdown"/>
+        </div>
+        <Button id="sendButton">Send Email</Button>
+    </StrictMode>
+  )
--- web/web.go
+++ web/web.go
@@ -11,13 +11,17 @@ var (
 	dist embed.FS
 	//go:embed dist/index.html
 	indexHTML embed.FS
+	//go:embed dist/test/test.html
+	testHTML embed.FS
 
 	distDirFS     = echo.MustSubFS(dist, "dist")
 	distIndexHTML = echo.MustSubFS(indexHTML, "dist")
+	distTestHTML  = echo.MustSubFS(testHTML, "dist/test")
 )
 
 // RegisterHandlers registers the web handlers to serve the frontend
 func RegisterHandlers(e *echo.Echo) {
 	e.FileFS("/", "index.html", distIndexHTML)
+	e.FileFS("/test", "test.html", distTestHTML)
 	e.StaticFS("/", distDirFS)
 }
